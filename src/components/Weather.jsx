bf7d5ab36a74977bd77e2a21d9435f742b8a5cd3
ilham-sh <ilhamsyauqih@outlook.co.id>
2025-12-08 22:28:45 +0700

    Adding search bar

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from '../assets/search.png'
 
 const Weather = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className='weather'>
+            <div className="search-bar">
+                <input type="text" placeholder='Search'/>
+                <img src={search_icon} alt="" />
+            </div>
         </div>
     )
 }
